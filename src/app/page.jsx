b265e3eebf6ca228beb97216be8f6b3b96afd400
JayKanjia <jaykanjia11@gmail.com>
2023-07-10 13:59:46 +0530

    card animation done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import Header from '@/components/Header';
+import style from './style.module.css';
 
 const links = [
     {
@@ -31,8 +32,12 @@ export default function Home() {
             <Header />
 
             <main className="relative min-h-[300px] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-                <div className="relative max-w-md w-[90vw] aspect-[7/4] rounded-2xl border transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-950/90 overflow-hidden">
-                    <div className="relative gap-8 w-full h-full bg-black">
+                <div
+                    className={`${style.cardWrapper} relative max-w-md w-[90vw] aspect-[7/4] rounded-2xl`}
+                >
+                    <div
+                        className={`${style.cardFront} relative gap-8 w-full h-full border transition-colors border-gray-300  dark:border-neutral-700 dark:bg-neutral-950/90 rounded-2xl overflow-hidden`}
+                    >
                         <Image
                             className="object-cover dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
                             src="/images/dummyCard.png"
@@ -42,15 +47,19 @@ export default function Home() {
                             priority
                         />
                     </div>
-                    <div className="absolute top-0 left-0 gap-8 w-full h-full">
-                        <Image
-                            className="object-cover dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-                            src="/images/dummyCardBack.png"
-                            alt="smart card image"
-                            fill
-                            size="100vw"
-                            priority
-                        />
+                    <div className="absolute top-0 left-0 w-full h-full -z-10">
+                        <div
+                            className={`${style.cardBack} relative  gap-8 w-full h-full border transition-colors border-gray-300 dark:border-neutral-700 dark:bg-neutral-950/90 rounded-2xl overflow-hidden`}
+                        >
+                            <Image
+                                className="object-cover dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+                                src="/images/dummyCardBack.png"
+                                alt="smart card image"
+                                fill
+                                size="100vw"
+                                priority
+                            />
+                        </div>
                     </div>
                 </div>
                 {/* <Image
