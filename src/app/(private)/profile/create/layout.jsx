9a54ab9f1f6f1c5a0fcc54ab969c5dd6cae78992
Jay Kanjia <73180746+jaykanjia@users.noreply.github.com>
2023-08-14 23:41:58 +0530

    Jk (#8)
    * changes in tailwind config
    
    * tailwind config changes
    
    * profile routing and 404 routing, copy link to clipboard
    
    * dashboard and theme set
    
    * vercel backend setup
    
    * commit
    
    * commit
    
    * multi part form
    
    * ui fixes and create profile form
    
    * commit
    
    * added create card page (previous form not deleted)
    
    * same as previous commit
    
    * create profile api call done
    
    * profile/create route reset and ui improved
    
    ---------
    
    Co-authored-by: Sky-walker-69 <100207624+Sky-walker-69@users.noreply.github.com>

--- src/app/(private)/profile/create/layout.jsx
+++ src/app/(private)/profile/create/layout.jsx
@@ -6,7 +6,7 @@ const layout = ({ children }) => {
             className={`w-full md:py-8 min-h-screen flex justify-center items-center bg-light-300 dark:bg-dark-700`}
         >
             <div
-                className={`max-w-screen-md w-full bg-light-500 sm:dark:bg-dark-500 dark:bg-dark-700 p-8 md:rounded-xl`}
+                className={`max-w-screen-md w-full bg-light-500 sm:dark:bg-dark-900 dark:bg-dark-700 p-8 md:rounded-xl`}
             >
                 {children}
             </div>
---
+++ src/components/CreateCard/index.jsx
@@ -0,0 +1,305 @@
+import React, { useRef } from 'react';
+import styles from './createcard.module.css';
+import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
+import ImagePicker from '../ImagePicker';
+import { toast } from 'react-toastify';
+import AxiosInstance from '@/AxiosInstance';
+
+const CreateCard = () => {
+    const router = useRouter();
+    const nameRef = useRef();
+    const designationRef = useRef();
+    const contactNumebrRef = useRef();
+    const emailRef = useRef();
+    const websiteRef = useRef();
+    const addressRef = useRef();
+    const aboutRef = useRef();
+    const youtubeRef = useRef();
+    const linkedinRef = useRef();
+    const instagramRef = useRef();
+    const whatsappRef = useRef();
+    const facebookRef = useRef();
+    const skypeRef = useRef();
+    const twitterRef = useRef();
+
+    const handleSubmitClick = async (e) => {
+        e.preventDefault();
+        try {
+            const data = {
+                data: {
+                    name: nameRef.current.value,
+                    designation: designationRef.current.value,
+                    about: aboutRef.current.value,
+                    socialLinks: [
+                        { title: 'youtube', link: youtubeRef.current.value },
+                        { title: 'linkedin', link: linkedinRef.current.value },
+                        {
+                            title: 'instagram',
+                            link: instagramRef.current.value,
+                        },
+                        { title: 'whatsapp', link: whatsappRef.current.value },
+                        { title: 'facebook', link: facebookRef.current.value },
+                        { title: 'skype', link: skypeRef.current.value },
+                        { title: 'twitter', link: twitterRef.current.value },
+                    ],
+                    contactDetails: {
+                        phoneNumber: contactNumebrRef.current.value,
+                        email: emailRef.current.value,
+                        address: addressRef.current.value,
+                        website: websiteRef.current.value,
+                    },
+                },
+            };
+            console.log(data);
+            const response = await AxiosInstance.post('/profile', data, {
+                headers: {
+                    'auth-token': sessionStorage.getItem('AUTH_TOKEN'),
+                },
+            });
+            console.log(response);
+            toast.success(response?.data?.message);
+        } catch (error) {
+            toast.error(error);
+        }
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmitClick}>
+                <div
+                    className={`flex justify-center text-gray-500 font-semibold`}
+                >
+                    WELCOME !
+                </div>
+                <div
+                    className={`text-2xl font-semibold flex justify-center py-4`}
+                >
+                    Create Your Profile
+                </div>
+                <div className="grid md:grid-cols-2 gap-8 items-center w-full">
+                    <div className={styles.inputContainer}>
+                        <span
+                            className={clsx([styles.details, styles.required])}
+                        >
+                            Name
+                        </span>
+                        <input
+                            ref={nameRef}
+                            className={styles.inputbox}
+                            type="text"
+                            placeholder="Enter your name"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span
+                            className={clsx([styles.details, styles.required])}
+                        >
+                            Designation
+                        </span>
+                        <input
+                            ref={designationRef}
+                            className={styles.inputbox}
+                            type="text"
+                            placeholder="Tell us about your work"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span
+                            className={clsx([styles.details, styles.required])}
+                        >
+                            Contact Number
+                        </span>
+                        <input
+                            ref={contactNumebrRef}
+                            className={styles.inputbox}
+                            type="text"
+                            placeholder="Enter your contact number"
+                            pattern="[0-9]{10}$"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span
+                            className={clsx([styles.details, styles.required])}
+                        >
+                            Email
+                        </span>
+                        <input
+                            ref={emailRef}
+                            className={styles.inputbox}
+                            type="email"
+                            placeholder="Enter your Company email address"
+                            required
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                            title="Please enter a valid email address"
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span
+                            className={clsx([styles.details, styles.required])}
+                        >
+                            Website Link
+                        </span>
+                        <input
+                            ref={websiteRef}
+                            className={styles.inputbox}
+                            type="url"
+                            placeholder="Enter your website"
+                            required
+                            title="Please enter your website link"
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span
+                            className={clsx([styles.details, styles.required])}
+                        >
+                            Address
+                        </span>
+                        <textarea
+                            ref={addressRef}
+                            className={styles.inputbox}
+                            type="text"
+                            required
+                            style={{
+                                resize: 'vertical',
+                                overflow: 'hidden',
+                            }}
+                            onInput={(e) => {
+                                e.target.style.height = 'auto';
+                                e.target.style.height =
+                                    e.target.scrollHeight + 'px';
+                            }}
+                        />
+                    </div>
+                    <div className={`styles.inputContainer md:col-span-2`}>
+                        <span
+                            className={clsx([styles.details, styles.required])}
+                        >
+                            About
+                        </span>
+                        <textarea
+                            ref={aboutRef}
+                            className={styles.inputbox}
+                            type="about"
+                            required
+                            style={{
+                                resize: 'vertical',
+                                overflow: 'hidden',
+                            }}
+                            onInput={(e) => {
+                                e.target.style.height = 'auto';
+                                e.target.style.height =
+                                    e.target.scrollHeight + 'px';
+                            }}
+                        />
+                    </div>
+
+                    <div className={styles.inputContainer}>
+                        <span className={styles.details}>YouTube</span>
+                        <input
+                            ref={youtubeRef}
+                            className={styles.inputbox}
+                            type="url"
+                            placeholder="LinkedIn Profile Link"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span className={styles.details}>LinkedIn</span>
+                        <input
+                            ref={linkedinRef}
+                            className={styles.inputbox}
+                            type="url"
+                            placeholder="YouTube Link"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span className={styles.details}>Instagram</span>
+                        <input
+                            ref={instagramRef}
+                            className={styles.inputbox}
+                            type="url"
+                            placeholder="Instagram Profile Link"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span className={styles.details}>WhatsApp</span>
+                        <input
+                            ref={whatsappRef}
+                            className={styles.inputbox}
+                            type="text"
+                            placeholder="WhatsApp Number"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span className={styles.details}>Facebook</span>
+                        <input
+                            ref={facebookRef}
+                            className={styles.inputbox}
+                            type="url"
+                            placeholder="Facebook Profile Link"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span className={styles.details}>Skype</span>
+                        <input
+                            ref={skypeRef}
+                            className={styles.inputbox}
+                            type="text"
+                            placeholder="SkypeId"
+                            required
+                        />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span className={styles.details}>Twitter</span>
+                        <input
+                            ref={twitterRef}
+                            className={styles.inputbox}
+                            type="url"
+                            placeholder="Twitter Profile Link"
+                            required
+                        />
+                    </div>
+                </div>
+
+                {/* <div className="flex justify-evenly">
+                        <div className={styles.inputContainer}>
+                            <span className={styles.details}>
+                                Profile Image
+                            </span>
+                            <ImagePicker />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <span className={styles.details}>Banner Image</span>
+                            <ImagePicker />
+                        </div>
+                    </div> */}
+
+                {/*buttons */}
+                <div className="m-[30px_0px] flex gap-3">
+                    <button
+                        className={`bg-red-500 text-white w-full hover:bg-red-400 hover:bg-opacity-70 rounded-md p-2`}
+                        onClick={() => router.back()}
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        className={`bg-primary text-white w-full hover:bg-primary hover:bg-opacity-70 rounded-md p-2`}
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </>
+    );
+};
+
+export default CreateCard;
