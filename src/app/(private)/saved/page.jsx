c960a64f5343f9e8482193069055758f083bf3fb
Jay Kanjia <jaykanjia11@gmail.com>
2023-11-08 18:02:39 +0530

    final commit

--- src/app/(private)/saved/page.jsx
+++ src/app/(private)/saved/page.jsx
@@ -6,7 +6,6 @@ import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import clsx from 'clsx';
 import styles from './styles.module.css';
-import { data } from 'autoprefixer';
 
 const Page = () => {
     const [profiles, setProfiles] = useState([]);
